0afa2acd629753d75977a78787752420a132b278
ZeyarHtet <zeyarhtet.zyh@gmail.com>
2024-12-02 15:15:24 +0630

    ui

---
+++ lib/common.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const maincolor = Colors.black;
+
+const seccolor = Colors.white;
+
+const backgroundcolor = Color.fromARGB(255, 250, 244, 244);
+
+const listcolor = Color.fromARGB(255, 75, 134, 77);
+
+const tabcolor = Color.fromARGB(255, 75, 134, 77);
+
+const btncolor = Color.fromARGB(255, 179, 177, 177);
+
+
+
+
